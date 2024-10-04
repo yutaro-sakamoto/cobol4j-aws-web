0c5836f1495846f9ed243bb00eb5ef7117494775
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-10-04 21:35:01 +0900

    [Add]: create a VPC

---
+++ infrastructure/lib/constructs/Network/index.ts
@@ -0,0 +1,50 @@
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as logs from "aws-cdk-lib/aws-logs";
+import * as iam from "aws-cdk-lib/aws-iam";
+
+/**
+ * VPCとVPCエンドポイントに関するリソースを定義する
+ */
+export class Network extends Construct {
+  /**
+   * VPC
+   */
+  public readonly vpc: ec2.Vpc;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    // VPCを作成
+    this.vpc = new ec2.Vpc(this, "Vpc", {
+      natGateways: 0,
+      createInternetGateway: false,
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "Private",
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ],
+    });
+
+    // VPC Flow Logsを作成
+    const vpcFlowLogGroup = new logs.LogGroup(this, "VpcFlowLogGroup", {
+      retention: logs.RetentionDays.THREE_DAYS,
+    });
+
+    const vpcFlowLogRole = new iam.Role(this, "VpcFlowLogGroupRole", {
+      assumedBy: new iam.ServicePrincipal("vpc-flow-logs.amazonaws.com"),
+    });
+
+    new ec2.FlowLog(this, "FlowLog", {
+      resourceType: ec2.FlowLogResourceType.fromVpc(this.vpc),
+      trafficType: ec2.FlowLogTrafficType.ALL,
+      destination: ec2.FlowLogDestination.toCloudWatchLogs(
+        vpcFlowLogGroup,
+        vpcFlowLogRole,
+      ),
+    });
+  }
+}
